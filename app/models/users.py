eecc547f2ba0bb7ff6adea42d9918c57bc6ba3ce
roddajohn <johnrod.john@gmail.com>
2017-03-13 14:04:03 -0400

    Added add_user, stuff not working

--- app/models/users.py
+++ app/models/users.py
@@ -11,6 +11,22 @@ def get_user(id):
     The dictionary retrieved from the database corresponding to the user with id <id>
     """
 
-    user = mongo.db.users.find_one({'id' : id})
+    user = mongo.db.users.find_one({'_id' : id})
 
     return user
+
+def add_user(user_data):
+    """ Adds a user to the database
+    
+    user_data: dictionary of user data
+    Keys:
+        TODO
+
+    Returns: 
+    id of user if success, 0 if failure
+    """
+
+    user_id = mongo.db.users.insert_one(user_data).inserted_id
+
+    return inserted_id
+    
--- tests/models/test_users.py
+++ tests/models/test_users.py
@@ -1,9 +1,13 @@
 """ Tests the users.py file
 
 Checks get_user(id)
+Check add_user(user_data)
 """
 
-from app.models.users import get_user
+from app.models.users import get_user, add_user
+
+def test_add_user():
+    assert not 0 == add_user({'username': 'johnrodjohn'})
 
 def test_get_user():
     assert None == get_user(0)
