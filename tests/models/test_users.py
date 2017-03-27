10864421d2adc4a7169cad2fc4a1f5a80db0d836
roddajohn <johnrod.john@gmail.com>
2017-03-27 15:24:40 -0400

    Added set and check password, along with unit tests

--- tests/models/test_users.py
+++ tests/models/test_users.py
@@ -5,11 +5,16 @@ Tests all fields for a user
 Tests simple querying
 """
 
-from app.extensions import db
+import pytest
 
+from app.extensions import db
 from app.models.users import User
 
 def test_users():
+    """ Basic User tests
+
+    Tests basic creation and saving of a user object, as well as basic querying """
+    
     new_user = User(fname = 'testing_fname', lname = 'testing_lname')
 
     db.session.add(new_user)
@@ -18,5 +23,23 @@ def test_users():
     query = User.query.filter_by(fname = 'testing_fname').order_by(User.id.desc()).first()
 
     assert query.id == new_user.id
+
+def test_password():
+    """ Basic tests on password hash
+
+    Setting password
+    Checking password 
+
+    No need to actually save this user to the database as this simply tests class methods"""
+
+    new_user = User(fname = 'testing_fname', lname = 'testing_lname')
+    
+    new_user.set_password('test')
+    
+    assert new_user.password is not None
+    assert new_user.check_password('test')
+    assert not new_user.check_password('TEST')
+
+    
 
     
