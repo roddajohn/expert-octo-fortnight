007928daba98a57065dfaa79c78e68674236e441
roddajohn <johnrod.john@gmail.com>
2017-04-01 04:01:17 -0400

    Added before request handler ofr setting g variable

--- app/core/authentication.py
+++ app/core/authentication.py
@@ -1,3 +1,22 @@
 """ File managing decorators managing who is logged in and not, as well as permission handling """
 
+from app.models.permissions import Role
+from app.models.users import User
 
+from app.application import app
+
+@app.before_request
+def load_user():
+    """ Loads the user from the session into the g variable 
+
+    If session does not contain id, sets g.user to None
+    """
+    
+    user = None
+    
+    if session['id']:
+        user = User.query.filter_by(id = session['id'])
+
+    g.user = user
+        
+        
