39d282e9c01700fbd712e0e3e068620f8800a9d1
roddajohn <johnrod.john@gmail.com>
2017-04-05 13:35:33 -0400

    Added unit tests for app.core.authentication

--- app/core/authentication.py
+++ app/core/authentication.py
@@ -1,7 +1,6 @@
 """ File managing decorators managing who is logged in and not, as well as permission handling as well as functions decorated with before_app_request """
 
-from flask import g
-from flask import session
+from flask import g, session, flash, redirect
 
 import logging
 
--- tests/core/test_authentication.py
+++ tests/core/test_authentication.py
@@ -10,8 +10,63 @@ from tests.conftest import app
 from app.extensions import db
 from app.core.authentication import load_user, require_login, require_role
 
+def test_require_login():
+    """ Tests the require_login decorator """
+
+    user = create_test_user()
+
+    db.session.add(user)
+    db.session.commit()
+    
+    def testing_function():
+        return 'Success'
+
+    with app.test_request_context(''): # Tests with proper id set
+        session['id'] = user.id # This is how the load_user method loads the user
+        app.preprocess_request()
+
+        assert require_login(testing_function)() == 'Success'
+        
+    with app.test_request_context(''): # Tests without proper id set
+        app.preprocess_request()
+        assert 'Redirecting' in require_login(testing_function)().data # Redirecting to 'index' as defined in app.core.authentication
+
+    db.session.delete(user)
+    db.session.commit()
+    
+def test_require_role():
+    """ Tests the require_role decorator """
+
+    user = create_test_user()
+    user.add_role('testing')
+
+    db.session.add(user)
+    db.session.commit()
+    
+    def testing_function():
+        return 'Success'
+
+    with app.test_request_context(''): # Tests with proper role
+        session['id'] = user.id # This is how the load_user method loads the user
+        app.preprocess_request()
+
+        assert (require_role(testing_function, 'testing')()) == 'Success'
+        
+    with app.test_request_context(''): # Tests without role
+        session['id'] = user.id # This is how the load_user method loads the user
+        app.preprocess_request()
+
+        user.remove_role('testing') # Removes the role to test without the desired role
+        db.session.commit()
+        
+        assert 'Redirecting' in require_role(testing_function, 'testing')().data # Redirecting to 'index' as defined in app.core.authentication
+
+    db.session.delete(user)
+    db.session.commit()
+
+
 def test_load_user():
-    """ Tests the load_user decorator """
+    """ Tests the load_user pre request processor """
 
     user = create_test_user()
     
@@ -30,5 +85,9 @@ def test_load_user():
         load_user()
         
         assert g.user == None
+
+    db.session.delete(user)
+    db.session.commit()
+    
 
     
