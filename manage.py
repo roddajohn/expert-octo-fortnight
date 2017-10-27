0104583b1beaf8fc4d80b0fb12ddf892575bf9b8
Rodda John <rj2485@columbia.edu>
2017-10-27 19:21:53 -0400

    FINAL SQL WORKING VERSION

--- manage.py
+++ manage.py
@@ -44,6 +44,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import scoped_session, sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
+# Updated
 # Application imports
 from app.application import create_app, get_config
 from app.extensions import db
